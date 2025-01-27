4c1b724e7b4359da2017f977cdaec320f331845e
jjlindsey <jlindsey010@gmail.com>
2025-01-27 10:45:53 -0800

    revising

--- script.js
+++ script.js
@@ -53,15 +53,15 @@ function showMovies(movies) {
     })
 }
 
-// function getClassByRate(vote) {
-//     if(vote >= 8){
-//         return 'green'
-//     }else if(vote >= 5) {
-//         return 'orange'
-//     } else {
-//         return 'red'
-//     }
-// }
+function getClassByRate(vote) {
+    if(vote >= 8){
+        return 'green'
+    }else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
